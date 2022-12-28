39b8317155a7d8bbdccbf42ffbac755b19d309f9
ozakiryota <kb22056@gmail.com>
2022-12-28 11:08:01 +0900

    Fix file paths

--- interact.sh
+++ interact.sh
@@ -12,10 +12,10 @@ docker run \
 	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--gpus all \
 	-v $HOME/dataset:/root/dataset \
-	-v $(pwd)/../mount/exp:/root/semseg/exp \
-	-v $(pwd)/../mount/train_airsim.sh:/root/semseg/tool/train_airsim.sh \
-	-v $(pwd)/../mount/airsim_pspnet50.yaml:/root/semseg/config/airsim/airsim_pspnet50.yaml \
-	-v $(pwd)/../mount/resnet50_v2.pth:/root/semseg/initmodel/resnet50_v2.pth \
-	-v $(pwd)/../mount/airsim_colors.txt:/root/semseg/data/airsim/airsim_colors.txt \
-	-v $(pwd)/../mount/airsim_names.txt:/root/semseg/data/airsim/airsim_names.txt \
+	-v $(pwd)/mount/exp:/root/semseg/exp \
+	-v $(pwd)/mount/train_airsim.sh:/root/semseg/tool/train_airsim.sh \
+	-v $(pwd)/mount/airsim_pspnet50.yaml:/root/semseg/config/airsim/airsim_pspnet50.yaml \
+	-v $(pwd)/mount/resnet50_v2.pth:/root/semseg/initmodel/resnet50_v2.pth \
+	-v $(pwd)/mount/airsim_colors.txt:/root/semseg/data/airsim/airsim_colors.txt \
+	-v $(pwd)/mount/airsim_names.txt:/root/semseg/data/airsim/airsim_names.txt \
 	$image:$tag
