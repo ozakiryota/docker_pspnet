63d058dac7a32928af611a3390b959aa5c9737e2
ozakiryota <kb22056@gmail.com>
2022-12-29 23:54:52 +0900

    Rename files

--- interact.sh
+++ interact.sh
@@ -13,10 +13,10 @@ docker run \
 	--gpus all \
 	-v $HOME/dataset:/root/dataset \
 	-v $(pwd)/mount/exp:/root/semseg/exp \
-	-v $(pwd)/mount/train_airsim.sh:/root/semseg/tool/train_airsim.sh \
-	-v $(pwd)/mount/test_airsim.sh:/root/semseg/tool/test_airsim.sh \
-	-v $(pwd)/mount/airsim_pspnet50.yaml:/root/semseg/config/airsim/airsim_pspnet50.yaml \
+	-v $(pwd)/mount/config/airsim:/root/semseg/config/airsim \
+	-v $(pwd)/mount/data/airsim:/root/semseg/data/airsim \
+	-v $(pwd)/mount/tool/airsim_train.sh:/root/semseg/tool/airsim_train.sh \
+	-v $(pwd)/mount/tool/airsim_test.sh:/root/semseg/tool/airsim_test.sh \
 	-v $(pwd)/mount/resnet50_v2.pth:/root/semseg/initmodel/resnet50_v2.pth \
-	-v $(pwd)/mount/airsim_colors.txt:/root/semseg/data/airsim/airsim_colors.txt \
-	-v $(pwd)/mount/airsim_names.txt:/root/semseg/data/airsim/airsim_names.txt \
+	-v $(pwd)/mount/my_tool:/root/semseg/my_tool \
 	$image:$tag
