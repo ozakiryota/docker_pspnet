7325cb45cd78441385973df0be1ca766c2577a82
ozakiryota <kb22056@gmail.com>
2023-01-15 23:31:54 +0900

    Fix script

--- tensorboard.sh
+++ tensorboard.sh
@@ -8,14 +8,15 @@ log_dir=`basename $@`
 
 image="pspnet"
 tag="latest"
-
-echo $@
+home_dir="/home/user"
 
 docker run \
 	-it \
 	--rm \
+	-e local_uid=$(id -u $USER) \
+	-e local_gid=$(id -g $USER) \
 	--net=host \
 	-p 6006:6006 \
-	-v $(pwd)/mount/exp:/root/semseg/mount/exp \
+	-v $(pwd)/mount/exp:$home_dir/semseg/mount/exp \
 	$image:$tag \
 	bash -c "tensorboard --logdir=$@"
